627583eff3d152621d5a26e64c2e8f50ea72a388
Lucas <lucasgabrielsantos398@gmail.com>
2024-01-21 18:03:39 -0300

    Alteração exercicio de File

--- src/ManipulationFileExercise/Program.java
+++ src/ManipulationFileExercise/Program.java
@@ -1,25 +1,50 @@
 package ManipulationFileExercise;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import ManipulationFileExercise.entities.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
-        String path = "C:\\Windows\\Temp\\in.csv";
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        List<Product> products =  new ArrayList<>();
+
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
+
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
+
+        boolean success = new File(sourceFolderStr + "\\out").mkdir();
+
+        String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
 
 
             String line = br.readLine();
-            System.out.println(line);
-            String[] line1 = line.split(",", 3);
-            for (String line2 : line1) {
-                System.out.println(line2);
+
+            while (line != null) {
+                String[] lineSplit = line.split(",");
+                String name = lineSplit[0];
+                double price = Double.parseDouble(lineSplit[1]);
+                int quantity = Integer.parseInt(lineSplit[2]);
+
+                products.add(new Product(name, price, quantity));
+
+                line = br.readLine();
 
             }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter()))
 
         }
         catch (IOException e) {
